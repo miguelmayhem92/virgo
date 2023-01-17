03ee648c28fb3dc8488ab0349ada8e6d08a8c337
Miguel <miguelmayhem92@gmail.com>
2023-01-17 21:43:28 +0100

    po tensorflow done, modeling baseline started

--- research_env/configs.py
+++ research_env/configs.py
@@ -1,7 +1,11 @@
-class data_configs():
-    n_days = 350
+class data_configs:
+    n_days = 950
     lags = 3
     window = 7
     std_column = 'Close_roll_std'
     ref_price = 'Close'
     logdif_column = 'Dif'
+    split_config = {
+        'train':0.65,
+        'val':0.85
+    }
